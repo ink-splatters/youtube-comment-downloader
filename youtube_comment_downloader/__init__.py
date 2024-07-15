cea4d27b3806a3236c3e9aedd8da96d23de48e19
Peter A. <ink.splatters@pm.me>
2024-07-15 23:53:37 +0200

    formatting

--- youtube_comment_downloader/__init__.py
+++ youtube_comment_downloader/__init__.py
@@ -6,36 +6,68 @@ import time
 from typing import Optional, Union
 
 import click
-from .downloader import YoutubeCommentDownloader, SORT_BY_POPULAR, SORT_BY_RECENT
+
+from .downloader import SORT_BY_POPULAR, SORT_BY_RECENT, YoutubeCommentDownloader
 
 
 def to_json(comment: dict, indent: Optional[int] = None) -> Union[str, bytes]:
     comment_str = json.dumps(comment, ensure_ascii=False, indent=indent)
     if indent is None:
         return comment_str
-    padding = ' ' * (2 * indent) if indent else ''
-    return ''.join(padding + line for line in comment_str.splitlines(True))
+    padding = " " * (2 * indent) if indent else ""
+    return "".join(padding + line for line in comment_str.splitlines(True))
 
 
 @click.command()
-@click.option('--youtubeid', '-y', help='ID of Youtube video for which to download the comments')
-@click.option('--url', '-u', help='Youtube URL for which to download the comments')
-@click.option('--output', '-o', required=True, help='Output filename (output format is line delimited JSON)')
-@click.option('--pretty/--no-pretty', '-p', default=False, help='Change the output format to indented JSON')
-@click.option('--limit', '-l', type=int, help='Limit the number of comments')
-@click.option('--language', '-a', type=str, default=None, help='Language for Youtube generated text (e.g. en)')
-@click.option('--sort', '-s', type=int, default=SORT_BY_RECENT,
-              help='Whether to download popular (0) or recent comments (1). Defaults to 1')
-def main(youtubeid: Optional[str], url: Optional[str], output: str, pretty: bool, limit: Optional[int], language: Optional[str], sort: int) -> None:
+@click.option(
+    "--youtubeid", "-y", help="ID of Youtube video for which to download the comments"
+)
+@click.option("--url", "-u", help="Youtube URL for which to download the comments")
+@click.option(
+    "--output",
+    "-o",
+    required=True,
+    help="Output filename (output format is line delimited JSON)",
+)
+@click.option(
+    "--pretty/--no-pretty",
+    "-p",
+    default=False,
+    help="Change the output format to indented JSON",
+)
+@click.option("--limit", "-l", type=int, help="Limit the number of comments")
+@click.option(
+    "--language",
+    "-a",
+    type=str,
+    default=None,
+    help="Language for Youtube generated text (e.g. en)",
+)
+@click.option(
+    "--sort",
+    "-s",
+    type=int,
+    default=SORT_BY_RECENT,
+    help="Whether to download popular (0) or recent comments (1). Defaults to 1",
+)
+def main(
+    youtubeid: Optional[str],
+    url: Optional[str],
+    output: str,
+    pretty: bool,
+    limit: Optional[int],
+    language: Optional[str],
+    sort: int,
+) -> None:
     """Download Youtube comments without using the Youtube API"""
     if (not youtubeid and not url) or not output:
-        raise ValueError('you need to specify a Youtube ID/URL and an output filename')
+        raise ValueError("you need to specify a Youtube ID/URL and an output filename")
 
     if os.path.sep in output:
         outdir = os.path.dirname(output)
         os.makedirs(outdir, exist_ok=True)
 
-    print('Downloading Youtube comments for', youtubeid or url)
+    print("Downloading Youtube comments for", youtubeid or url)
     downloader = YoutubeCommentDownloader()
     generator = (
         downloader.get_comments(youtubeid, sort, language)
@@ -44,27 +76,36 @@ def main(youtubeid: Optional[str], url: Optional[str], output: str, pretty: bool
     )
 
     count = 1
-    with io.open(output, 'w', encoding='utf8') as fp:
+    with io.open(output, "w", encoding="utf8") as fp:
         start_time = time.time()
 
         if pretty:
-            fp.write('{\n' + ' ' * 4 + '"comments": [\n')
+            fp.write("{\n" + " " * 4 + '"comments": [\n')
 
         comment = next(generator, None)
         while comment:
             comment_str = to_json(comment, indent=4 if pretty else None)
-            comment = None if limit and count >= limit else next(generator, None)  # Note that this is the next comment
-            comment_str = comment_str + ',' if pretty and comment is not None else comment_str
-            print(comment_str.decode('utf-8') if isinstance(comment_str, bytes) else comment_str, file=fp, end='')
-            sys.stdout.write(f'\rDownloaded {count} comment(s)')
+            comment = (
+                None if limit and count >= limit else next(generator, None)
+            )  # Note that this is the next comment
+            comment_str = (
+                comment_str + "," if pretty and comment is not None else comment_str
+            )
+            print(
+                comment_str.decode("utf-8")
+                if isinstance(comment_str, bytes)
+                else comment_str,
+                file=fp,
+                end="",
+            )
+            sys.stdout.write(f"\rDownloaded {count} comment(s)")
             sys.stdout.flush()
             count += 1
 
         if pretty:
-            fp.write('\n' + ' ' * 4 + ']\n}')
-    print(f'\n[{time.time() - start_time:.2f} seconds] Done!')
+            fp.write("\n" + " " * 4 + "]\n}")
+    print(f"\n[{time.time() - start_time:.2f} seconds] Done!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
--- youtube_comment_downloader/__main__.py
+++ youtube_comment_downloader/__main__.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
 if __package__ is None:
-    import sys, os.path
+    import os.path
+    import sys
+
     path = os.path.realpath(os.path.abspath(__file__))
     sys.path.insert(0, os.path.dirname(os.path.dirname(path)))
 
 import youtube_comment_downloader
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     youtube_comment_downloader.main()
